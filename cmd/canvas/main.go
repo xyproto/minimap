18fc091005362f38a4b362a4930831ce14bb0ebb
Alexander F. Rødseth <rodseth@gmail.com>
2023-10-25 22:58:10 +0200

    Add a function that uses the vt100.Canvas

---
+++ cmd/canvas/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/xyproto/minimap"
+	"github.com/xyproto/mode"
+	"github.com/xyproto/vt100"
+)
+
+func main() {
+	filename := ""
+	if len(os.Args) > 1 {
+		filename = os.Args[1]
+	}
+
+	if filename == "" {
+		log.Println("please provide a filename as the first argument")
+		os.Exit(1)
+	}
+
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+
+	// Use mode.SimpleDetect to detect the mode based on the file contents
+	fileMode := mode.SimpleDetectBytes(data)
+
+	// Initialize vt100 terminal settings
+	vt100.Init()
+	defer vt100.Close() // ensure that terminal settings are reset when we're done
+
+	// Prepare a canvas
+	c := vt100.NewCanvas()
+
+	// Draw minimap onto the canvas
+	var cw = int(c.Width())
+	var ch = int(c.Height())
+
+	const margin = 2
+
+	const width = 20
+	var height = ch - (margin * 2)
+
+	// The x and y position for where the minimap should be drawn
+	var xpos = cw - (width + margin)
+	const ypos = margin
+
+	err = minimap.DrawMinimap(c, string(data), xpos, ypos, width, height, fileMode, 7, vt100.DarkGray, vt100.Black, vt100.LightYellow, vt100.BackgroundBlack)
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+
+	// Draw the contents of the canvas
+	c.Draw()
+
+	// Wait for a keypress
+	vt100.WaitForKey()
+}
--- colors.go
+++ colors.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/xyproto/mode"
 	"github.com/xyproto/textoutput"
+	"github.com/xyproto/vt100"
 )
 
 // ColorMinimap returns a colored text representation of the given text.
@@ -24,6 +25,13 @@ func ColorMinimap(data string, width, height int, m mode.Mode, highlightIndex in
 	var result strings.Builder
 	o := textoutput.New()
 
+	determineColor := func(char string, m mode.Mode) string {
+		if char == " " {
+			return "black" // spaces between tokens in the minimap
+		}
+		return "darkgray" // default color
+	}
+
 	for i := 0; i < min(len(lines), height*heightStep); i += heightStep {
 		for j := 0; j < min(len(lines[i]), width*widthStep); j += widthStep {
 			color := determineColor(string(lines[i][j]), m)
@@ -41,9 +49,34 @@ func ColorMinimap(data string, width, height int, m mode.Mode, highlightIndex in
 	return result.String(), nil
 }
 
-func determineColor(char string, m mode.Mode) string {
-	if char == " " {
-		return "black" // spaces between tokens in the minimap
+// DrawMinimap draws a colored representation of the given text onto a vt100.Canvas.
+// x and y are the starting coordinates on the canvas.
+// width and height are the number of characters and lines for the minimap.
+// highlightIndex is the line index to be highlighted in the minimap. Use -1 for no highlight.
+func DrawMinimap(c *vt100.Canvas, data string, x, y, width, height int, m mode.Mode, highlightIndex int, contentColor, spaceColor, highlightColor, bgColor vt100.AttributeColor) error {
+	if width <= 0 || height <= 0 {
+		return errors.New("width and height must both be positive integers")
 	}
-	return "darkgray" // default color
+
+	lines := strings.Split(data, "\n")
+
+	widthStep := max(1, len(lines[0])/width)
+	heightStep := max(1, len(lines)/height)
+
+	for i := 0; i < min(len(lines), height*heightStep); i += heightStep {
+		for j := 0; j < min(len(lines[i]), width*widthStep); j += widthStep {
+			char := string(lines[i][j])
+			color := contentColor
+			if char == " " {
+				color = spaceColor
+			}
+			if i <= highlightIndex && highlightIndex < i+heightStep {
+				c.WriteRune(uint(x+j/widthStep), uint(y+i/heightStep), highlightColor, bgColor, '█')
+			} else {
+				c.WriteRune(uint(x+j/widthStep), uint(y+i/heightStep), color, bgColor, '█')
+			}
+		}
+	}
+
+	return nil
 }
